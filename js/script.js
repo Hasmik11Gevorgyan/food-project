240f1ea33cdbbac355f5a64bea3ca0498ca5f0cd
unknown <you@example.com>
2025-03-14 19:46:09 +0400

    we have done timer logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict"
 
 window.addEventListener("DOMContentLoaded", function(){
+  // Tab logic start
   const tabsHeaders = document.querySelectorAll(".tabheader__item");
   const tabsContents = document.querySelectorAll(".tabcontent");
   const tabsHeadersParent = document.querySelector(".tabheader__items");
@@ -34,6 +35,54 @@ window.addEventListener("DOMContentLoaded", function(){
       });
     }
   });
+  // tab logic end 
+
+  // timer logic start
+ const deadline = "2025-03-17"
+  function getTimeRemaining(endtime){
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(total / (1000*60*60*24));
+    const hours = Math.floor(total / (1000*60*60) %24);
+    const minutes = Math.floor((total /1000/60) %60);
+    const secunds = Math.floor((total/1000) %60);
+    return{
+      total,
+      days,
+      hours,
+      minutes,
+      secunds,
+    }
+  }
+ function setZero(n){
+   return n >= 0 && n <10 ? `0${n}` : n
+ }
+
+  function setClock(selector, endtime){
+    const timer = document.querySelector(selector);
+    const daysBlock = document.querySelector("#days");
+    const hoursBlock = document.querySelector("#hours");
+    const minutesBlock = document.querySelector("#minutes");
+    const secundsBlock = document.querySelector("#seconds");
+
+    const timerId = setInterval(upDateClock,1000)
+
+    function upDateClock(){
+      const {total,days, hours,minutes,secunds} = getTimeRemaining(endtime)
+      
+      daysBlock.textContent = setZero(days )
+      hoursBlock.textContent = setZero(hours )
+      minutesBlock.textContent = setZero(minutes ) 
+      secundsBlock.textContent = setZero(secunds )
+
+      if(total <= 0){
+        clearInterval(timerId)
+      }
+    }
+    upDateClock()
+    
+  }
+setClock(".timer",deadline)
+  // timer logic end
 });
 
 
